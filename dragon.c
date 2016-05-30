306516a5717765a0e9eccce8fa980ffc097158c4
B46qqq <b46qqq@hotmail.com>
2016-05-31 00:41:07 +1000

    Add files via upload

---
+++ dragon.c
@@ -0,0 +1,29 @@
+/* This program is designed to display a dragon !!! 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+	printf(           "                       ``      -`   \n" ); 
+	printf("                                 .s.    .:/      \n");
+	printf("                               `:s+   `-/o`      \n" );
+	printf( "                    `/:    `.:+ys` `-:+s.        \n");
+	printf("               .` `:soo:///ooyhs+:-:+ss-`.` `    \n");
+	printf("              -ys+ydhhhdmyso+///+++shs+/+o::/  ``\n");
+	printf("    .`      /+/shyyhhyyyddhsooooosyhso++oy+:..-:/.\n");
+	printf("  .s. .+/+ohyyysosss+ommyhhhyhhhyyyyoo+oy+/+//:` \n");
+	printf("  .oyo+ssoyooyhoossysyoysoyhyssssooososyoso/+:+:` \n");
+	printf(" .odhdhossos++ssoo++/+//ohmhyo/:/ooossos+o+:+/:.  \n");
+	printf(" oyhhsooo+++/o///+::/++o/yddhhy+::++o+///+///-`   \n");
+	printf(" yssyos++/////oo/+osyy+o+/ohdhoo:-/////:--....    \n");
+	printf(" ys++/+:-....`oh/++oo++/-:/o::/+/-/::--..````     \n");
+	printf(" -s/  ``   `/yhy+/:/::::.:``  /+::`     ``        \n");
+	printf("  `        /sso/:/::::-.     `/:-`                \n");
+	printf("          .yoo/:----/-     .:/-`                  \n");
+	printf("         `+++/:-../:      .-`                     \n");
+	printf("        `s:--..:+.          `                    \n");
+                       
+
+	return 0;
+
+}
