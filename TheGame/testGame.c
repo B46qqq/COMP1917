091c7d4ec9af382a92e148199ef96de88a71c5d5
B46qqq <b46qqq@hotmail.com>
2016-05-31 13:51:55 +1000

    Update testGame.c

--- TheGame/testGame.c
+++ TheGame/testGame.c
@@ -39,61 +39,22 @@ int main (int argc, char * argv[]) {
    testDiceValue (g, CoDiceValue);
    testGetCampus (g);
    testGetARC (g);
-   printf("All tests passed, you are Awesome!\n");
+   printf ("All tests passed, you are Awesome!\n");
 
    return EXIT_SUCCESS;
 }
 
 void testGetDiscipline (Game ptrGame, int *studentType) { // testing with known values
+// Using loops are really efficient then writing hundreds of asserts functions
    for (int i = 0; i < 19; i++) {
       assert (getDiscipline (ptrGame, i) == studentType[i]);
    }
-/*
-   assert (getDiscipline (ptrGame, 0)  == STUDENT_BQN);
-   assert (getDiscipline (ptrGame, 1)  == STUDENT_MMONEY);
-   assert (getDiscipline (ptrGame, 2)  == STUDENT_MJ);
-   assert (getDiscipline (ptrGame, 3)  == STUDENT_MMONEY);
-   assert (getDiscipline (ptrGame, 4)  == STUDENT_MJ);
-   assert (getDiscipline (ptrGame, 5)  == STUDENT_BPS);
-   assert (getDiscipline (ptrGame, 6)  == STUDENT_MTV);
-   assert (getDiscipline (ptrGame, 7)  == STUDENT_MTV);
-   assert (getDiscipline (ptrGame, 8)  == STUDENT_BPS);
-   assert (getDiscipline (ptrGame, 9)  == STUDENT_MTV);
-   assert (getDiscipline (ptrGame, 10) == STUDENT_BQN);
-   assert (getDiscipline (ptrGame, 11) == STUDENT_MJ);
-   assert (getDiscipline (ptrGame, 12) == STUDENT_BQN);
-   assert (getDiscipline (ptrGame, 13) == STUDENT_THD);
-   assert (getDiscipline (ptrGame, 14) == STUDENT_MJ);
-   assert (getDiscipline (ptrGame, 15) == STUDENT_MMONEY);
-   assert (getDiscipline (ptrGame, 16) == STUDENT_MTV);
-   assert (getDiscipline (ptrGame, 17) == STUDENT_BQN);
-   assert (getDiscipline (ptrGame, 18) == STUDENT_BPS);*/
 }
 
 void testDiceValue (Game ptrGame, int *diceValue) {
    for (int i = 0; i < 19; i++) {
       assert (getDiceValue (ptrGame, i) == diceValue[i]);
    }
-/*
-   assert (getDiceValue (ptrGame, 0) == 9);
-   assert (getDiceValue (ptrGame, 1) == 10);
-   assert (getDiceValue (ptrGame, 2) == 8);
-   assert (getDiceValue (ptrGame, 3) == 12);
-   assert (getDiceValue (ptrGame, 4) == 6);
-   assert (getDiceValue (ptrGame, 5) == 5);
-   assert (getDiceValue (ptrGame, 6) == 3);
-   assert (getDiceValue (ptrGame, 7) == 11);
-   assert (getDiceValue (ptrGame, 8) == 3);
-   assert (getDiceValue (ptrGame, 9) == 11);
-   assert (getDiceValue (ptrGame, 10) == 4);
-   assert (getDiceValue (ptrGame, 11) == 6);
-   assert (getDiceValue (ptrGame, 12) == 4);
-   assert (getDiceValue (ptrGame, 13) == 7);
-   assert (getDiceValue (ptrGame, 14) == 9);
-   assert (getDiceValue (ptrGame, 15) == 2);
-   assert (getDiceValue (ptrGame, 16) == 8);
-   assert (getDiceValue (ptrGame, 17) == 10);
-   assert (getDiceValue (ptrGame, 18) == 5);*/
 }
 
 void testGetCampus (Game ptrGame) {
